82b6dfcba5180dc86f3ab7b820f7dfab119c2367
ZhongweiMA <106167140+nihouhuima@users.noreply.github.com>
2023-04-13 11:50:47 +0200

    edit add topic

--- src/components/AddOneTopic.vue
+++ src/components/AddOneTopic.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="edit_change"> 
+        <router-link :to="{name: 'EditTopic'}"><img id="edit_return" src="../assets/return.png" alt="Return"/></router-link>
+        
         <div class="edit_add_engage_topic"> 
             <div class="edit_menu_text"> 
                 <p class="edit_title"><b>Engage Topic</b> </p>
             </div>
-            <table> 
+            <table id="edit_add_topic_table"> 
                 <tr> 
                     <td><p>Short Name :</p></td><td><input v-model="shortName" type="text"></td>
                 </tr>
@@ -20,26 +22,41 @@
                 <p class="edit_title"><b>Keywords</b> </p>
             </div>
             <table>
-                <tr><td><p>Language :</p></td><td><select v-model="lang"><option>English</option><option>Bulgarian</option><option>Dutch</option><option>Finnish</option><option>French</option><option>Italian</option><option>German</option><option>Norwegian</option><option>Spanish</option></select></td></tr>
+                <tr><td><p>Language :</p></td><td><select v-model="lang" >
+                    <option v-for="lan in list_lang" :key="lan">{{lan}}</option></select></td></tr>
             </table>
             
         
             <div class="edit_new_words"> 
 
-                <div v-for="(key, index) in keywords" :key="index" class="edit_add_type">
-                    <img class="edit_delete_type" @click="deleteOne(index)" src="../assets/POUBELLE3.png" />
-                    <div></div>
-                    <p>Type</p>
-                    <select v-model="key.type">
-                        <option disabled value="">Please select one type</option>
-                        <option v-for="type_el in types" :key="type_el" :label="type_el" :value="type_el" :name="type_el"></option>
-                    </select> 
-                    <p>Text <span>  (*please separate the words by ",")</span></p><textarea v-model="key.text" rows="4" cols="100"></textarea>
+               
                     
-                </div>
-
-                <p id="edit_add_set_keywords" @click="addOne()">+ Add a new set of keywords</p>
-            
+                    <div>
+                        <table><tr><td><p>Type : Single words</p></td>
+                        </tr>
+                        </table><br>
+                        <p id="edit_espace"> Words<span>  (*please separate the words by ",")</span></p>
+                        <textarea v-model="single_words" rows="4" cols="100"></textarea>
+                    </div>  
+                    <div>
+                        <table><tr><td><p>Type : At least</p></td>
+                        </tr>
+                        </table><br>
+                        <p id="edit_espace"> Words<span>  (*please separate the words by ",")</span></p>
+                        <textarea v-model="at_least" rows="4" cols="100"></textarea>
+                    </div>  
+                    <div>
+                        <table><tr><td><p>Type : Combined with</p></td>
+                        </tr>
+                        </table><br>
+                        <p id="edit_espace"> Words<span>  (*please separate the words by ",")</span></p>
+                        <textarea v-model="combined_with" rows="4" cols="100"></textarea>
+                    </div>  
+                    <!-- <img class="edit_delete_type" @click="deleteOne(index)" src="../assets/trash.svg" /> -->
+                
+                <!-- <p id="edit_add_set_keywords" @click="addOne()">+ Add a new set of keywords</p> -->
+            <button @click="confirm()">Confirm</button>
+            <button @click="cancel()">cancel</button>
             </div>
         
         </div>
@@ -51,10 +68,11 @@
 
 
 
-        <router-link :to="{name: 'EditTopic'}"><button>Return</button></router-link>
+        
     </div>
 </template>
 <script>
+
 export default {
     name: 'AddOneTopic',
     data(){
@@ -63,32 +81,37 @@ export default {
             shortName:"",
             FullName:"",
             lang:"English",
-            keywords:[
-                {
-                    type:"",
-                    text:""
-                }
-            ],
-            types:[
-                "Single words",
-                "At least",
-                "Combined with"
-            ]
+            list_lang:["English","Bulgarian","Dutch","Finnish","French","Italian","German","Norwegian","Spanish"],
+            single_words:"",
+            at_least:"",
+            combined_with:""
+
         }
     },
     methods:{
         getRouterData() {
             this.topic_type = this.$route.query.topic_type
         },
-        addOne(){
-            this.keywords.push({type:"",text:""});
-        },
-        deleteOne(index){
-            this.keywords.splice(index, 1);
-        }
+        // addOne(){
+        //     this.keywords.push({type:"",text:""});
+        //     this.TypesList();
+        // },
+        // deleteOne(index){
+        //     this.keywords.splice(index, 1);
+        //     this.TypesList();
+        // },
+       confirm(){
+
+       },
+       cancel(){
+        
+       }
     },
     created(){
         this.getRouterData();
+    }, 
+    computed:{
+        
     }
 
     
