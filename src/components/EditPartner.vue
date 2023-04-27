79cd6753716978d84303a279099af0d26eef02d9
ZhongweiMA <106167140+nihouhuima@users.noreply.github.com>
2023-04-27 15:37:05 +0200

    delete partner

--- src/components/EditPartner.vue
+++ src/components/EditPartner.vue
@@ -9,10 +9,11 @@
 <!-- {{ partners }} -->
         <table class="edit_table">
             <tr>
-                <td>Modify</td><td>University</td>
+                <td>Modify</td><td>Delete</td><td>University</td>
             </tr>
             <tr class="edit_tr" v-for="element in partners" :key="element.shortName">
                 <td class="edit_img_center"><img src="../assets/crayon.png" alt="edit" class="edit_img" @click="choosedata(element)"></td>
+                <td class="edit_img_center"><img src="../assets/gomme.png" alt="delete" class="edit_img" @click="deletePartner(element)"> </td>
                 <td><span>{{element.longName}}</span></td>
             </tr>
                 
@@ -56,6 +57,52 @@ export default {
                 query: this.topic
                 
             })
+        },
+        deletePartner(element){
+            this.$confirm(`Do you want to delete the university "${element.longName}"?`, "confirmation", {
+                iconClass : "el-icon-question",
+                confirmButtonText: "Yes",
+                cancelButtonText: "No",
+                showClose: true, // Whether or not to display the top right hand corner close button
+                type: "warning"
+                })
+                .then(()=>{this.delete(element)}) // to do : success ou failure based on return value
+                .catch((error)=>{
+                    this.$notify.error({
+                                title: "failure",
+                                message: error,
+                                duration: 1500
+                            })
+            })
+
+        },
+        delete(element){
+            const path=`${this.GLOBAL.BASE_URL}deletePartner/${element.shortName}`
+            axios.post(path)
+            .then((res) => {
+                this.flag=res.data;
+                    if(res.data==true || res.data=="true"){
+                        this.getdata()
+                        this.$notify({
+                            type:"success",
+                            message:"successfully deleted",
+                            duration: 1500
+                            })
+                    }else{
+                        this.$notify.error({
+                            title: "failure",
+                            message: "operation failed",
+                            duration: 1500
+                        })
+                    } 
+            })
+            .catch((error)=>{
+                this.$notify.error({
+                            title: "failure",
+                            message: error,
+                            duration: 1500
+                        })
+            }) 
         }
     },
     created(){
